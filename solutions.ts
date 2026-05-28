3c24c8287a41b95ee76d743977e76269eac7cff0
greenFruits <mdabdullah6548ctg@gmail.com>
2026-05-28 20:59:47 +0600

    added the solution of problem solving 06

--- solutions.ts
+++ solutions.ts
@@ -43,3 +43,26 @@ function toggleReadStatus(book: Book): Book & { isRead: boolean } {
     isRead: true,
   };
 }
+
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+class Student extends Person {
+  grade: string;
+
+  constructor(name: string, age: number, grade: string) {
+    super(name, age);
+    this.grade = grade;
+  }
+
+  getDetails(): string {
+    return `"Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}"`;
+  }
+}
