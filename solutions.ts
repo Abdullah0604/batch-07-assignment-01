a5e0aecee8f56c382ac14f7f765367a1814c61a7
greenFruits <mdabdullah6548ctg@gmail.com>
2026-05-28 20:02:46 +0600

    added the solution of problem solving 01.

---
+++ solutions.ts
@@ -0,0 +1,9 @@
+function filterEvenNumbers(numbers: number[]): number[] {
+  const evenNumbers: number[] = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) evenNumbers.push(numbers[i]);
+  }
+
+  return evenNumbers;
+}
